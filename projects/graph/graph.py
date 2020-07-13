5c6ca64f3796d0208875cf6359e24ddc7e39a74e
Chelsea Wetzel <chelswetz@gmail.com>
2020-07-13 18:17:33 -0400

    part 1

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,26 +13,29 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()  # TODO - this will hold edges 
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)  # TODO - there's an edge from v1 to v2
+        else:
+            raise IndexError("nonexistent vertex")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id] 
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+          # TODO
 
     def dft(self, starting_vertex):
         """
